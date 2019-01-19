8c868e46954135a28a67d902a0679061c82ad899
BilelJegham <bilel@jegham.com>
2019-01-19 22:08:03 +0100

    fonction

--- blocks/nosFonctions.js
+++ blocks/nosFonctions.js
@@ -16,6 +16,15 @@ Blockly.Constants.Text.HUE = 160;
 Blockly.defineBlocksWithJsonArray([  // BEGIN JSON EXTRACT
   // Block for text value
   {
+    "type": "changerCouleurTous",
+    "message0": "Changer couleur de tous en %1",
+    "args0": [ {
+      "type": "field_colour",
+      "name": "COULEUR",
+      "text": ""
+    }]
+  },
+  {
     "type": "changerCouleur",
     "message0": "Changer couleur de %1 en %2",
     "args0": [{
@@ -25,12 +34,201 @@ Blockly.defineBlocksWithJsonArray([  // BEGIN JSON EXTRACT
       "value": 0
     },
     {
-      "type": "field_input",
+      "type": "field_colour",
       "name": "COULEUR",
-      "check": "String",
       "text": ""
-    }],
-    "colour": "%{BKY_TEXTS_HUE}",
-    "helpUrl": "%{BKY_TEXT_TEXT_HELPURL}",
-    "tooltip": "%{BKY_TEXT_TEXT_TOOLTIP}",
-  }])
+    }]
+  },
+  {
+    "type": "allumer",
+    "message0": "Allumer %1",
+    "args0": [{
+      "type": "field_number",
+      "name": "ID",
+      "check": "Number",
+      "value": 0
+    }]
+  },
+  {
+    "type": "eteindre",
+    "message0": "Eteindre %1 ",
+    "args0": [{
+      "type": "field_number",
+      "name": "ID",
+      "check": "Number",
+      "value": 0
+    }]
+  },
+  {
+    "type": "Animer",
+    "message0": "Animer %1",
+    "args0": [{
+      "type": "field_number",
+      "name": "ID",
+      "check": "Number",
+      "value": 0
+    }]
+  },
+  {
+    "type": "AnimerTous",
+    "message0": "Animer tous",
+  }
+  /**
+   * 
+   * CAPTEUR BP
+   * 
+   */,
+  {
+    "type": "capteur_bp_status",
+    "message0": "Statut Capteur Btns",
+    "output": "String"
+  },
+  {
+    "type": "capteur_bp/switch/ledX/state",
+    "message0": "Etat led %1",
+    "output": "String",
+    "args0": [{
+      "type": "field_number",
+      "name": "ID",
+      "check": "Number",
+      "value": 0
+    }]
+  },
+  {
+    "type": "capteur_bp/binary_sensor/bpX/state",
+    "message0": "Etat bouton %1 ",
+    "output": "String",
+    "args0": [{
+      "type": "field_number",
+      "name": "ID",
+      "check": "Number",
+      "value": 0
+    }]
+  },
+  {
+    "type": "capteur_bp/switch/ledX/command/allumer",
+    "message0": "Allumer led %1 ",
+    "output": "String",
+    "args0": [{
+      "type": "field_number",
+      "name": "ID",
+      "check": "Number",
+      "value": 0
+    }]
+  },
+  {
+    "type": "capteur_bp/switch/ledX/command/eteindre",
+    "message0": "Eteindre led %1 ",
+    "output": "String",
+    "args0": [{
+      "type": "field_number",
+      "name": "ID",
+      "check": "Number",
+      "value": 0
+    }]
+  }
+  /**
+   * Télécommande infrarouge
+   */
+  ,
+  {
+    "type": "remote/NOM_DE_LA_TOUCHE/state",
+    "message0": "Etat bouton telecommande %1 ",
+    "output": "String",
+    "args0": [{
+      "type": "field_input",
+      "name": "NAME_BTN",
+      "check": "String"
+    }]
+  }
+
+  /**
+   * Capteur détecteur de présence
+   */
+  ,
+  {
+    "type": "presence/state",
+    "message0": "Etat détection",
+    "output": "String"
+  } 
+    /**
+   * Capteur de distance
+   */
+  ,
+  {
+    "type": "distance/value",
+    "message0": "Etat détection",
+    "output": "Number"
+  } 
+  /**
+   * Capteur atmosphérique
+   */
+  ,
+  {
+    "type": "atmosphere/temperature",
+    "message0": "Temperature",
+    "output": "Number"
+  } 
+  ,
+  {
+    "type": "atmosphere/pression",
+    "message0": "Pression",
+    "output": "Number"
+  } 
+  ,
+  {
+    "type": "atmosphere/humidite",
+    "message0": "Humidite Absolu",
+    "output": "Number"
+  } 
+  ,
+  {
+    "type": "atmosphere/humidite_absolue",
+    "message0": "Humidite Absolu",
+    "output": "Number"
+  }   
+  /**
+  * musique
+  */
+ ,
+ {
+   "type": "music/control/getstate",
+   "message0": "Etat Musique",
+   "output": "String"
+ } ,
+ {
+   "type": "music/control/getvol",
+   "message0": "Volume",
+   "output": "Number"
+ },
+ {
+   "type": "music/control/next",
+   "message0": "Suivant"
+ },
+ {
+   "type": "music/control/previous",
+   "message0": "Précédent"
+ },
+ {
+   "type": "music/control/stop",
+   "message0": "Arreter"
+ },
+ {
+   "type": "music/control/play",
+   "message0": "Jouer"
+ },
+ {
+   "type": "music/control/pause",
+   "message0": "Pause"
+ },
+ {
+   "type": "music/control/setvol",
+   "message0": "Toogle",
+   "args0": [{
+    "type": "field_input",
+    "name": "VOLUME",
+    "check": "Number"
+  }]
+ }
+
+])
--- generators/python/nosFonctions.js
+++ generators/python/nosFonctions.js
@@ -6,8 +6,47 @@ goog.provide('Blockly.Python.nosFonctions');
 goog.require('Blockly.Python');
 
 
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? "["+result[1]+","+result[2]+","+result[3]+"]": null;
+}
+
+Blockly.Python['changerCouleurTous'] = function(block) {
+    var code = "changerCouleurTous("+hexToRgb(block.getFieldValue('COULEUR'))+")";
+    return code;
+   };
+
+
 Blockly.Python['changerCouleur'] = function(block) {
- // Text value.
- var code = "changerCouleur("+block.getFieldValue('ID')+","+Blockly.Python.quote_(block.getFieldValue('COULEUR'))+")";
+ var code = "changerCouleur("+block.getFieldValue('ID')+","+hexToRgb(block.getFieldValue('COULEUR'))+")";
  return code;
 };
+
+
+Blockly.Python['eteindre'] = function(block) {
+    var code = "changerCouleur("+block.getFieldValue('ID')+",[0,0,0])";
+    return code;
+   };
+
+Blockly.Python['allumer'] = function(block) {
+    var code = "changerCouleur("+block.getFieldValue('ID')+",[255,255,255])";
+    return code;
+   };
+   
+Blockly.Python['animer'] = function(block) {
+    var code = "animer("+block.getFieldValue('ID')+")";
+    return code;
+};
+
+Blockly.Python['animerTous'] = function(block) {
+    var code = "animerTous()";
+    return code;
+};
+
+Blockly.Python['capteur_bp_status'] = function(block) {
+    var code = "getStatusBP()";
+    return code;
+};
+
+   
